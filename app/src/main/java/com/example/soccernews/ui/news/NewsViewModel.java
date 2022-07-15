60050c14d164c50df790084d0a2b6bc3c69ca1f9
AndreGomez <andyatonement@hotmail.com>
2022-07-15 19:06:32 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -0,0 +1,31 @@
+package com.example.soccernews.ui.news;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.soccernews.domain.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsViewModel extends ViewModel {
+
+    private final MutableLiveData<List<News>> news;
+
+    public NewsViewModel() {
+        news = new MutableLiveData<>();
+
+        //TODO Remover Mock de Notícias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Ferroviáia Tem Desfalque importante", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"));
+        news.add(new News("Ferrinha Joga no Sábado", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"));
+        news.add(new News("Copa do Mundo Feminina Está Terminando", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"));
+
+        this.news.setValue(news);
+    }
+
+    public LiveData<List<News>> getNews() {
+        return news;
+    }
+}
