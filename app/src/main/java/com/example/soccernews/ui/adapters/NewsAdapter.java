ca16bff06d9a56c4fd348970f104ef5bb003e432
AndyAtonement <andyatonement@hotmail.com>
2022-07-27 21:39:42 -0300

    Applying changes

--- app/src/main/java/com/example/soccernews/ui/adapters/NewsAdapter.java
+++ app/src/main/java/com/example/soccernews/ui/adapters/NewsAdapter.java
@@ -3,6 +3,7 @@ package com.example.soccernews.ui.adapters;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -17,9 +18,11 @@ import java.util.List;
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private List<News> news;
+    private View.OnClickListener favoritesListener;
 
-    public NewsAdapter(List<News> news) {
+    public NewsAdapter(List<News> news, View.OnClickListener favoritesListener) {
         this.news = news;
+        this.favoritesListener = favoritesListener;
     }
 
     @NonNull
@@ -33,14 +36,25 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         News news = this.news.get(position);
-        holder.binding.tvTitle.setText(news.getTitle());
-        holder.binding.tvDescription.setText(news.getDescription());
-        Picasso.get().load(news.getImage()).fit().into(holder.binding.ivThumbnail);
+        holder.binding.tvTitle.setText(news.title);
+        holder.binding.tvDescription.setText(news.description);
+        Picasso.get().load(news.image).fit().into(holder.binding.ivThumbnail);
+        //Implementação da funcionalidade de "Abrir Link"
         holder.binding.btnOpenlink.setOnClickListener(view -> {
             Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(news.getLink()));
+            i.setData(Uri.parse(news.link));
             holder.itemView.getContext().startActivity(i);
         });
+        //Implementação da funcionalidade de Compartilhar"
+        holder.binding.ivShare.setOnClickListener(view -> {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_SUBJECT, news.title);
+            i.putExtra(Intent.EXTRA_TEXT, news.link);
+            holder.itemView.getContext().startActivity(Intent.createChooser(i, "Share"));
+        });
+        //Implementação da funcionalidade de Favoritar" (O evento será instanciado pelo fragment)
+        holder.binding.ivFavorites.setOnClickListener(this.favoritesListener);
     }
 
     @Override
